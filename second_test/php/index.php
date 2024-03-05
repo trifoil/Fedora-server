c0d210082d1f78c4f0caf3eedea95b7fda64535c
trifoil <unixsslayer@gmail.com>
2024-03-06 00:53:00 +0100

    a

--- second_test/php/index.php
+++ second_test/php/index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blank Page</title>
+    <title>PHP Test Page</title>
 </head>
 <body>
-    <h1>Site2</h1>
+    <h1>Hello, World!</h1>
+    <p>This is a simple PHP test page.</p>
+    <p>Current date and time: <?php echo date("Y-m-d H:i:s"); ?></p>
 </body>
 </html>
