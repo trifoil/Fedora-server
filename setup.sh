0022d3bc2f0a991e70260f7debdfb92622b487f3
trifoil <unixsslayer@gmail.com>
2024-03-05 19:08:20 +0100

    a

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" >&2
+  exit 1
+fi
+
+echo "The script will now install your tools"
+
+apt update
+
+# configuring cockpit
+
+apt install -y cockpit 
+systemctl enable --now cockpit.socket
+
+sudo firewall-cmd --permanent --zone=public --add-service=cockpit
+sudo firewall-cmd --reload
+
+# 
+
+echo "Installation complete"
