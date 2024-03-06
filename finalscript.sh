6e3de524f7e1b3c58331a8333a4487efdaf270d6
trifoil <unixsslayer@gmail.com>
2024-03-06 18:28:18 +0100

    a

---
+++ finalscript.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" >&2
+  exit 1
+fi
+
+echo "The script will now install your containers"
+
+dnf update
+
+# install and enable docker (start auto at boot)
+
+dnf install docker -y
+systemctl enable docker
+systemctl start docker
+
+
+# to not need to put sudo for using docker with my administrator user 
+
+usermod -aG docker administrator
+
+# and to not need to put sudo for your current user
+
+usermod -aG docker $USER
+
+# install docker-compose
+
+dnf install docker-compose
+systemctl enable docker-compose
+
+
+# pull and run the portainer image it needs to be privileged because it is conflicting SELinx
+
+docker volume create portainer_data
+docker run -d --privileged -p 9443:9443 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+docker --version
+
+
+
+echo "Installation complete"
+
--- initial_setup.sh
+++ initial_setup.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-echo "The script will now install your tools"
+echo "The script will now install your basic tools"
 
 dnf update
 
@@ -22,33 +22,6 @@ firewall-cmd --reload
 echo You can now connect at :
 
 ip a | grep -E 'inet\s' | grep -v '127.0.0.1' | awk '{print $2}' | awk -F/ '{print $1}'
-
-# install and enable docker (start auto at boot)
-
-dnf install docker -y
-systemctl enable docker
-
-systemctl start docker
-
-
-# to not need to put sudo for using docker with my administrator user 
-
-usermod -aG docker administrator
-
-# and to not need to put sudo for your current user
-
-usermod -aG docker $USER
-
-# install docker-compose
-
-dnf install docker-compose
-
-# pull and run the portainer image it needs to be privileged because it is conflicting SELinx
-
-docker volume create portainer_data
-docker run -d --privileged -p 9443:9443 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-docker --version
 
 
 echo "Installation complete"
