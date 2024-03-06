b083dc162526f59ce5256b9fb5a9c6160dd91072
trifoil <unixsslayer@gmail.com>
2024-03-06 19:28:11 +0100

    a

--- 1_docker_setup.sh
+++ 1_docker_setup.sh
@@ -30,6 +30,10 @@ systemctl enable docker-compose
 docker volume create portainer_data
 docker run -d --privileged -p 9443:9443 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+# display docker version
 
 docker --version
+
+
+
 echo "Installation complete"
