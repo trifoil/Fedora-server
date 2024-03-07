03bb2a666496763d18fc4420f6232128b36ba7a0
trifoil <unixsslayer@gmail.com>
2024-03-07 01:16:36 +0100

    update for phpmyadmin and mysql

--- 3_apache-php-fpm-app/app/index.php
+++ 3_apache-php-fpm-app/app/index.php
@@ -6,7 +6,23 @@
 </head>
 <body>
 	<?php 
-	echo "<h1>NOW I'M FEELING SO FLAT LIKE A G6</h1>"; 
+		echo "<h1>NOW I'M FEELING SO FLAT LIKE A G6</h1>"; 
+	?>
+	<?php
+		$host = 'mysql-db';
+		$user = 'db_user';
+		$pass = 'password';
+		$db = 'test_database';
+
+		$conn = new mysqli($host, $user, $pass, $db);
+
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		echo "Connected to MySQL successfully";
+
+		$conn->close();
 	?>
 </body>
 </html>
