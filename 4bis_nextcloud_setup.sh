7f2ff022b73852f3e4ede356277eadb3898cdf70
trifoil <unixsslayer@gmail.com>
2024-03-24 22:37:15 +0100

    a

--- 4bis_nextcloud_setup.sh
+++ 4bis_nextcloud_setup.sh
@@ -17,7 +17,7 @@ sudo docker run \
 --name nextcloud-aio-mastercontainer \
 --restart always \
 --publish 80:80 \
---publish 8080:8080 \
+--publish 8888:8080 \
 --publish 8443:8443 \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
