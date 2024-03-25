90d11de373a43223b8291c9e2b62b7b4a967afdd
trifoil <unixsslayer@gmail.com>
2024-03-25 02:42:50 +0100

    a

--- 4_nextcloudAIO/config.php
+++ 4_nextcloudAIO/config.php
@@ -1,8 +1,21 @@
 <?php
 $CONFIG = array (
+
+
   'trusted_domains' =>
   array (
     0 => 'localhost',
     1 => 'nextcloud.trifoil.cloudns.nz',
   ),
 );
+
+/* <?php
+$CONFIG = array (
+'trusted_domains' =>
+   [
+    '192.168.1.52',
+    'localhost',
+    'nextcloud.trifoil.cloudns.nz',
+    '192.168.1.52:8888'
+  ],
+); */
