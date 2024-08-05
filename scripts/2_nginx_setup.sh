a07027920c26780ebdb43ff97153fba576d2f628
trifoil <unixsslayer@gmail.com>
2024-08-06 00:52:52 +0200

    removed annoying sh

--- scripts/2_nginx_setup.sh
+++ scripts/2_nginx_setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root" >&2
-  exit 1
-fi
-
-
 echo "The script will now install nginx proxy manager"
 echo "Updating..."
 dnf update -y
--- scripts/3_php_site_setup.sh
+++ scripts/3_php_site_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root" >&2
-  exit 1
-fi
-
 echo "The script will now install your php website"
 echo "Updating..."
 dnf update -y
