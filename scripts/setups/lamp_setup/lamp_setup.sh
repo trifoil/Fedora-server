8f635804c719bdb510926719040b6bc9be00243e
trifoil <unixsslayer@gmail.com>
2024-08-06 01:58:57 +0200

    a

--- scripts/setups/lamp_setup/lamp_setup.sh
+++ scripts/setups/lamp_setup/lamp_setup.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+echo "The script will now install your php website"
+echo "Updating..."
+dnf update -y
+
+docker-compose up --build -d
+
+docker ps
+
+read -n 1 -s -r -p "Done. Press any key to continue..."
