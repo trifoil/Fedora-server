8b7c9fa76d22c2ddd15f73d02e582d69fa0faf6b
trifoil <unixsslayer@gmail.com>
2024-08-06 01:51:45 +0200

    a

--- setup.sh
+++ setup.sh
@@ -12,7 +12,10 @@ display_menu() {
     echo -e "|                   ${BLUE}Welcome to the setup assistant ${NC}                    |"
     echo "|                Please select the tool you want to use                |"
     echo "|----------------------------------------------------------------------|"
-    echo "| 0. Setup NGINX Proxy Manager                                         |"
+    echo "| 0. Setup Portainer                                                   |"
+    echo "| 1. Setup NGINX Proxy Manager                                         |"
+    echo "| 2. Setup LAMP (Linux, Apache, MySQL, PHP)                            |"
+    echo "| 3. Setup Nextcloud AIO                                               |"
     echo "|----------------------------------------------------------------------|"
     echo "| q. Exit                                                              |"
     echo "|----------------------------------------------------------------------|"
@@ -20,7 +23,19 @@ display_menu() {
 }
 
 setup_npm(){
-    sh scripts/setups/nginx_compose/nginx_setup.sh
+    sh scripts/setups/nginx_setup/nginx_setup.sh
+}
+
+setup_portainer(){
+    sh scripts/setups/portainer_setup/portainer_setup.sh
+}
+
+setup_lamp(){
+    sh scripts/setups/lamp_setup/lamp_setup.sh
+}
+
+setup_nextcloud_aio(){
+    sh scripts/setups/nextcloud_aio_setup/nextcloud_aio_setup.sh
 }
 
 main() {
@@ -28,7 +43,10 @@ main() {
         display_menu
         read -p "Enter your choice: " choice
         case $choice in
-            0) display_running ;;
+            0) setup_portainer ;;
+            1) setup_npm ;;
+            2) setup_lamp ;;
+            3) setup_nextcloud_aio ;;
             q) echo "Bye loser!" && clear && exit;;
             *) clear && echo "Invalid choice. Please enter a valid option." ;;
         esac
