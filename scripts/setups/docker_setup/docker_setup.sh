bb3baa5a406671d5ffe18cf3a5d3335f04969923
trifoil <unixsslayer@gmail.com>
2024-08-07 00:04:24 +0200

    docker setup

---
+++ scripts/setups/docker_setup/docker_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dnf -y remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+dnf -y install dnf-plugins-core
+dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+
+read -n 1 -s -r -p "Done. Press any key to continue..."
