a8cefdc4a976f1d0d1222c7f119644b2dcf01a4a
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-11-23 00:47:34 +0100

    firewall for vpn

--- scripts/setup/outline_vpn_setup/outline_vpn_setup.sh
+++ scripts/setup/outline_vpn_setup/outline_vpn_setup.sh
@@ -1,3 +1,12 @@
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --permanent --add-port=36255/tcp
+sudo firewall-cmd --permanent --add-port=7893/tcp
+sudo firewall-cmd --permanent --add-port=7893/udp
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-ports
+
+
 sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
 
 read -n 1 -s -r -p "Done. Press any key to continue..."
